334cbf65553f4746a4814b57f33f90ac3ad9c244
Rajshriwagh <mahajan.rajshri16@gmail.com>
2022-09-30 22:20:21 +0530

    ADD: Added logic for 2nd Max number program

--- src/main/java/com/JavaGenerics/App.java
+++ src/main/java/com/JavaGenerics/App.java
@@ -1,13 +1,27 @@
 package com.JavaGenerics;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Hello to Genrics Program!");
+        logger.info("Give 3 inputs:");
+        Scanner sc = new Scanner(System.in);
+        MaxInteger i = new MaxInteger();
+        int inp1 = Integer.parseInt(sc.nextLine());
+        int inp2 = Integer.parseInt(sc.nextLine());
+        int inp3 = Integer.parseInt(sc.nextLine());
+        int ArrayOfInputs[] = { inp1, inp2, inp3 };
+        int max = i.calculateMaximum(ArrayOfInputs);
+        i.checkMaxAt2ndPos(ArrayOfInputs[1], max);
     }
 }
