5cc3f4f84fc6fb471c83e29af40c3a3680114b82
Rajshriwagh <mahajan.rajshri16@gmail.com>
2022-09-30 18:58:12 +0530

    ADD: Added logic for 2nd Max number program

---
+++ src/main/java/com/JavaGenerics/MaxInteger.java
@@ -0,0 +1,27 @@
+package com.JavaGenerics;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MaxInteger {
+	private static final Logger logger = LogManager.getLogger(App.class);
+
+    int calculateMaximum(int[] ArrayOfInputs) {
+        int max = ArrayOfInputs[0];
+        if (ArrayOfInputs[1] > max) {
+            max = ArrayOfInputs[1];
+        }
+        if (ArrayOfInputs[2] > max) {
+            max = ArrayOfInputs[2];
+        }
+        return max;
+    }
+
+    void checkMaxAt2ndPos(int val, int max) {
+        if (val == max) {
+            logger.info("Max Number is at Position 2! " + max);
+        } else {
+            logger.info("Max Number is not present at Position 2! " + max);
+        }
+    }
+
+}
